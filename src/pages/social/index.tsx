791dcff5367c4629032204a0c820205d3eabdbc3
noahscott23 <nscott05@calpoly.edu>
2024-10-17 16:27:11 +0200

    FriendCard

--- src/pages/social/index.tsx
+++ src/pages/social/index.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import Link from "next/link";
-import Header from "@/components/Header";
+import React from 'react';
+import Header from '@/components/Header';
+import FriendCard from "@/components/FriendCard";
 
 const Social = () => {
+  const friends = ['Mateen', 'Jack', 'Noah', 'Owen']; 
+
   return (
     <div>
-      <Header></Header>
+      <Header />
+
+      <div className="relative bg-darkPurple top-10 rounded-lg p-6 ml-20 mr-20">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-white text-2xl">My Friends</h2>
+          <button className="bg-purple text-white py-2 px-4 rounded hover:bg-purple-600">
+            View All
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
+          {friends.map((friend) => (
+            <FriendCard key={friend} name={friend} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
