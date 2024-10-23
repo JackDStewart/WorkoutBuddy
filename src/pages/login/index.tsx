795fe8ea44849a779ad270360e199839c61730f3
noahscott23 <nscott05@calpoly.edu>
2024-10-23 11:57:49 +0200

    signup window

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,12 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 export default function LoginPage() {
+  const auth0Domain = "dev-tl8gmur5p3a4r3sh.us.auth0.com";
+  const clientId = "UueqTuat9GR9sZ17gSXSRjYiFF6WXqkC";
+  const callbackUrl = "http://localhost:3000/api/auth/callback";
+
+  const signupUrl = `https://${auth0Domain}/authorize?client_id=${clientId}&redirect_uri=${callbackUrl}&response_type=code&scope=openid profile email&screen_hint=signup`;
+
   return (
     <div>
       <div className="flex flex-col items-center mt-8">
@@ -38,13 +44,12 @@ export default function LoginPage() {
           <span className="text-white text-l"> 
               Don’t have an account?&nbsp;
             </span>
-            <Link
-              href={"/home"} 
-              passHref
+            <a
+              href = {signupUrl}
               className="text-white text-l underline transition duration-300"
             >
               Signup
-            </Link>
+            </a>
             </div>
         </div>
       </div>
