6f49766543d5ff7cd5f46b578f41987a001d3ed2
noahscott23 <nscott05@calpoly.edu>
2024-10-30 15:19:01 +0100

    try catch in GET

--- frontend/src/app/api/auth/[auth0]/route.js
+++ frontend/src/app/api/auth/[auth0]/route.js
@@ -1,4 +1,10 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = async (req, res) => {
+  try {
+    await handleAuth()(req, res);
+  } catch (error) {
+    console.error("Error in Auth0 handleAuth:", error);
+  }
+};
