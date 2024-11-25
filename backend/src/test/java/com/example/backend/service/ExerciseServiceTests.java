1b45d555fc332bbcff456bbc2d0ac914e4a94f1d
owenmarcione <omarcion@calpoly.edu>
2024-11-25 10:43:50 +0100

    working tests

--- backend/src/test/java/com/example/backend/service/ExerciseServiceTests.java
+++ backend/src/test/java/com/example/backend/service/ExerciseServiceTests.java
@@ -7,9 +7,9 @@ import com.example.backend.entity.enums.Equipment;
 import com.example.backend.entity.enums.MuscleGroup;
 import com.example.backend.repository.ExerciseRepository;
 import com.example.backend.repository.UserRepository;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -17,13 +17,14 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigInteger;
-import java.util.*;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
-
 @ExtendWith(MockitoExtension.class)
-public class ExerciseServiceTests {
+class ExerciseServiceTests {
 
     @InjectMocks
     ExerciseService exerciseService;
@@ -38,66 +39,70 @@ public class ExerciseServiceTests {
     BigInteger userId = new BigInteger("12345678901234");
     ExerciseDTO exerciseDTO = new ExerciseDTO("testName", "BARBELL", "QUADS", mockId);
     User user = new User("testName", "testEmail", userId);
+    Exercise exercise = new Exercise("testName", Equipment.NONE, MuscleGroup.CHEST, user);
+    List<Exercise> exercises = List.of(exercise);
 
-    @Test
-    public void getExercisesByUserIdTests() {
-        // Arrange
-        BigInteger userId = new BigInteger("12345678901234");
-        Exercise exercise = new Exercise();
-        List<Exercise> exercises = new ArrayList<>();
-        exercises.add(exercise);
+    @Nested
+    class getExerciseByUserIdTests {
+        @Test
+        void getExerciseByUserIdTest_UserExist() {
+            when(exerciseRepository.findByCreatedByIsNullOrUserId(Mockito.any())).thenReturn(exercises);
 
-        // Mocking behavior
-        when(exerciseRepository.findByCreatedByIsNullOrUserId(userId)).thenReturn(exercises);
+            List<Exercise> foundExercises = exerciseService.getExercisesByUserIdOrNull(mockId);
+            Assertions.assertEquals(1, foundExercises.size());
+            Assertions.assertEquals(exercises, foundExercises);
+            Assertions.assertEquals(exercise, foundExercises.getFirst());
 
-        // Act
-        List<Exercise> exerciseList = exerciseService.getExercisesByUserIdOrNull(userId.toString());
+            Mockito.verify(exerciseRepository, Mockito.times(1)).findByCreatedByIsNullOrUserId(Mockito.any());
+        }
 
-        // Assert
-        Assertions.assertNotNull(exerciseList);
-        Assertions.assertEquals(exercises.size(), exerciseList.size());
+        @Test
+        void getExerciseByUserIdTest_UserNull() {
+            when(exerciseRepository.findByCreatedByIsNullOrUserId(Mockito.any())).thenReturn(exercises);
 
-        // Verify
-        Mockito.verify(exerciseRepository, Mockito.times(1)).findByCreatedByIsNullOrUserId(userId);
+            List<Exercise> foundExercises = exerciseService.getExercisesByUserIdOrNull(mockId);
+            Assertions.assertEquals(1, foundExercises.size());
+            Assertions.assertEquals(exercises, foundExercises);
+            Assertions.assertEquals(exercise, foundExercises.getFirst());
+
+            Mockito.verify(exerciseRepository, Mockito.times(1)).findByCreatedByIsNullOrUserId(Mockito.any());
+        }
     }
 
-    @Test
-    public void createExerciseSuccessTest() {
-        // Arrange
-        Exercise exercise = new Exercise();
-        exercise.setName("testName");
-        exercise.setEquipment(Equipment.fromDisplayName("BARBELL"));
-        exercise.setMuscleGroup(MuscleGroup.fromDisplayName("QUADS"));
-        exercise.setCreatedBy(user);
+    @Nested
+    class createExerciseTest {
+        @Test
+        void createExerciseTestSuccess() {
+            // Arrange
 
-        // Mocking behavior
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+            // Mocking behavior
+            when(userRepository.findById(Mockito.any())).thenReturn(Optional.of(user));
+            when(exerciseRepository.save(Mockito.any(Exercise.class))).thenReturn(exercise);
 
-        // Act
-        Exercise testExercise = exerciseService.createExercise(exerciseDTO);
+            // Act
+            Exercise testExercise = exerciseService.createExercise(exerciseDTO);
 
-        // Assert
-        Assertions.assertNotNull(testExercise);
-        Assertions.assertEquals(exercise, testExercise);
+            // Assert
+            Assertions.assertNotNull(testExercise);
+            Assertions.assertEquals(exercise, testExercise);
 
-        // Verify
-        Mockito.verify(userRepository, Mockito.times(1)).findById(userId);
+            // Verify
+            Mockito.verify(userRepository, Mockito.times(1)).findById(userId);
+        }
+
+        @Test
+        public void createExerciseFailureTest() {
+
+            // Mocking behavior
+            when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+            // Act Assert
+            Exception e = Assertions.assertThrows(NoSuchElementException.class, () -> exerciseService.createExercise(exerciseDTO));
+
+            Assertions.assertEquals("create failed from Auth0 ID: " + mockId, e.getMessage());
+
+            // Verify
+            Mockito.verify(userRepository, Mockito.times(1)).findById(userId);
+        }
     }
-
-    @Test
-    public void createExerciseFailureTest() {
-
-        // Mocking behavior
-        when(userRepository.findById(userId)).thenReturn(Optional.empty());
-
-        // Act Assert
-        Exception e = Assertions.assertThrows(NoSuchElementException.class, () -> exerciseService.createExercise(exerciseDTO));
-
-        Assertions.assertEquals("create failed from Auth0 ID: " + mockId, e.getMessage());
-
-        // Verify
-        Mockito.verify(userRepository, Mockito.times(1)).findById(userId);
-    }
-
 }
-
